5642de3600d6a716d48431b5c43c758777cfe647
aero9560 <abhi.bhardwaj122@gmail.com>
2022-03-03 00:04:05 +0530

    Update e1.py

--- e1.py
+++ e1.py
@@ -24,66 +24,79 @@ def move(current_floor, desired_floor):
 
 
 elev_floor = []
-
-
 def elev_button(desired_floor):
     elev_floor.append(desired_floor)
 
 
-uplist = []
-downlist = []
+external = []
+def floor_button(x):
+    external.append(x)
 
+#elevator_no = int(input("enter no. of elevators"))
 
-def floor_button(desired_floor, direction):
-
-    if direction == 1:  # direction ==1 is going up
-        uplist.append(desired_floor)
-
-    else:
-        downlist.append(desired_floor)
-
-# elevator_no = int(input("enter no. of elevators"))
-
-
+# provide initial data
 total_floor = int(input("Enter no. of floors  "))
-start_floor = int(input(f"Elevator staring floor between 1 and {total_floor}  "))
-if start_floor == 1:
-    dir_elevator = 1
-elif start_floor == total_floor:
-    dir_elevator = 0
-else:
-    dir_elevator = int(input("Enter direction of elevator 1 for upward direction 0 for downward  "))
+starting_floor = []
 
-no_des = int(input("Enter no. of destinations from inside elevator "))
-for i in range(1, no_des + 1):
-    destination_floor = int(input("Enter floor number  "))
-    elev_button(destination_floor)
-
+for i in range(1,3):
+    floor_direction = []
+    start_floor = int(input(f"Enter  Elevator {i} staring floor between 1 and {total_floor}  "))
+    if start_floor == 1:
+        dir_elevator = 1
+    elif start_floor == total_floor:
+        dir_elevator = 0
+    else:
+            dir_elevator = int(input("Enter direction of elevator 1 for upward direction 0 for downward  "))
+    floor_direction.append(start_floor)
+    floor_direction.append(dir_elevator)
+    starting_floor.append(floor_direction)
+#print(starting_floor)
+for j in range(1,3):
+    no_des = int(input(f"Enter no. of destinations from inside elevator {j} "))
+    destination_list = []
+    for _ in range(1, no_des + 1):
+        destination_floor = int(input("Enter floor number  "))
+        destination_list.append(destination_floor)
+    destination_list = list(set(destination_list))
+    elev_button(destination_list)
+#print(elev_floor)
 des = int(input("Enter no. of destinations from floors "))
-for i in range(1, des+1):
+for _ in range(1, des+1):
+    ext = []
     des_floor = int(input("Enter floor no  "))
     direc = int(input("Enter direction of elevator call button ( 1 for upward direction 0 for downward ) "))
-    floor_button(des_floor, direc)
+    ext.append(des_floor)
+    ext.append(direc)
+    floor_button(ext)
+#print(external)
+external_destination = [[], []]
+for i in range(2):
+    if len(external) != 0:
+        for k in range(len(external)):
+            if starting_floor[i][1] == 1 and starting_floor[i][1] == external[k][1] and external[k][0] > starting_floor[i][0]:
+                external_destination[i].append(external[k][0])
+            elif starting_floor[i][1] == 0 and starting_floor[i][1] == external[k][1] and external[k][0] < starting_floor[i][0]:
+                external_destination[i].append(external[k][0])
+for i in range(len(external_destination)):
+    for k in range(len(external)):
+        if external_destination[i][0] == external[k][0]:
+            del external[k]
+            break
+#print(external_destination)
+#print(external)
+for i in range(len(external)):
 
-elev_floor = list(set(elev_floor))
-uplist = list(set(uplist))
-downlist = list(set(downlist))
-
-
-elev_floor.extend(uplist)
-elev_floor.extend(downlist)
-# print(elev_floor)
-elev_floor = list(set(elev_floor))
-# print(elev_floor)
-desired_floor_uplist = []
-desired_floor_downlist = []
-for k in range(len(elev_floor)):
-    if elev_floor[k] > start_floor:
-        desired_floor_uplist.append(elev_floor[k])
+    a = abs(external[i][0] - external_destination[0][-1])
+    b = abs(external[i][0] - external_destination[1][-1])
+    if a < b:
+        external_destination[0].append(external[i][0])
     else:
-        desired_floor_downlist.append(elev_floor[k])
-    desired_floor_uplist.sort()
-    desired_floor_downlist.sort(reverse=True)
+        external_destination[1].append(external[i][0])
+
+#print(external_destination)
+            #desired_floor_downlist.append(elev_floor[k])
+   # desired_floor_uplist.sort()
+   #desired_floor_downlist.sort(reverse=True)'''
 elev_floor.clear()
 if dir_elevator == 1:
     elev_floor.extend(desired_floor_uplist)
@@ -95,3 +108,4 @@ else:
 # print(elev_floor)
 move(start_floor, elev_floor)
 print("elevator is idel")
+
